b93110f22b78bb0effbeca22b5f83057ae26be7c
Luther <luther0812@163.com>
2017-01-22 23:56:26 +0800

    Java Chapter01

---
+++ Test_01/Chapter_Test_007.java
@@ -0,0 +1,15 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Test_01;
+
+/**
+ * 
+ *
+ */
+public class Chapter_Test_007 {
+	public static void main(String[] args){
+		System.out.println(4*(1.0-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)+(1.0/13)));
+	}
+
+}
