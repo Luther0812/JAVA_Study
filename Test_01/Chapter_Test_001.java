71888da4b4398f38729899eed42f62a6a8370dba
Luther <luther0812@163.com>
2017-01-23 00:15:21 +0800

    Java Chapter 01

---
+++ Test_01/Chapter_Test_001.java
@@ -0,0 +1,17 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Test_01;
+
+/**
+ * 
+ *
+ */
+public class Chapter_Test_001 {
+	public static void main(String[] agrs){
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Sience");
+		System.out.println("Programming is fun");
+	}
+
+}
---
+++ Test_01/Chapter_Test_004.java
@@ -0,0 +1,19 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Test_01;
+
+/**
+ * 
+ *
+ */
+public class Chapter_Test_004 {
+	public static void main(String[] args){
+		System.out.println("a     A^2     a^3");
+		System.out.println("1     1       1");
+		System.out.println("2     4       8");
+		System.out.println("3     9       27");
+		System.out.println("4     16      64");
+	}
+
+}
