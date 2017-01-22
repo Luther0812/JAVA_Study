52d6b6675be2fdce92e95de281ad202565fd3840
Luther <luther0812@163.com>
2017-01-23 00:23:31 +0800

    Java Chapter 02

---
+++ Program_02/SalesTax_007.java
@@ -0,0 +1,23 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Program_02;
+
+/**
+ *
+ *
+ */
+import java.util.Scanner;
+
+public class SalesTax_007 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter purchase amount: ");
+		double purchaseAmount = input.nextDouble();
+		
+		double tax = purchaseAmount * 0.06;
+		System.out.println("Sales tax is " + (int)(tax * 100) / 100.0);
+	}
+
+}
