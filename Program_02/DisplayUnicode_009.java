4e93215e6e261f4f5d1b1ad25a683fe5a07f2a24
Luther0812 <luther0812@163.com>
2017-01-23 15:03:38 +0800

    Java Chapter 02

---
+++ Program_02/DisplayUnicode_009.java
@@ -0,0 +1,18 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Program_02;
+
+/**
+ *
+ *
+ */
+import javax.swing.JOptionPane;
+
+public class DisplayUnicode_009 {
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null, "\u6B22\u8FCE \u03b1 \u03b2 \u03b3",
+				"\u6B22\u8FCE Welcome",JOptionPane.INFORMATION_MESSAGE);
+	}
+
+}
