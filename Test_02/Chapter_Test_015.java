44d2ddd6248b8feb44bcd6f68abc831343b5d78c
Luther <luther0812@163.com>
2017-02-21 23:49:27 +0800

    Java Chapter 02

---
+++ Test_02/Chapter_Test_015.java
@@ -0,0 +1,32 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Test_02;
+
+/**
+ *
+ *
+ */
+import java.util.Scanner;
+
+public class Chapter_Test_015 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter amount: ");
+		double amount = input.nextDouble();
+		System.out.print("Enter annualRate: ");
+		double annualRate = input.nextDouble();
+		
+		double monthlyRate = annualRate / 1200;
+		
+		double firstAmount = amount * (1 + monthlyRate);
+		double secondAmount = (amount + firstAmount) * (1 + monthlyRate);
+		double thirdAmount = (amount + secondAmount) * (1 + monthlyRate);
+		double fourthAmount = (amount + thirdAmount) * (1 + monthlyRate);
+		double fifthAmount = (amount + fourthAmount) * (1 + monthlyRate);
+		double sixthAmount = (amount + fifthAmount) * (1 + monthlyRate);
+		
+		System.out.println("The Sixth Month Amount is: " + sixthAmount);
+	}
+
+}
