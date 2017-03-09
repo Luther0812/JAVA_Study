a390c98fe91adecc236fa89a1f298a926660cad3
Luther0812 <luther0812@163.com>
2017-03-09 14:54:43 +0800

    Java Chapter 13

---
+++ Test_13/Chapter_Test_009.java
@@ -0,0 +1,48 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Test_13;
+
+/**
+ * @author up
+ *
+ */
+import java.util.Scanner;
+
+public class Chapter_Test_009 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		try{
+			System.out.print("Enter a string: ");
+			String s = input.nextLine();
+			for(int i = 0; i < s.length(); i++){
+				char t = s.charAt(i);
+				if(t == '0' || t== '1'){
+					
+				}else{
+					throw new BinaryFormatException();
+				}
+			}
+			System.out.println(s + "'s binary is " + binaryToDecimal(s));
+		}catch(BinaryFormatException ex){
+			System.out.println(ex);
+		}
+	}
+	
+	public static int binaryToDecimal(String s){
+		int num = 0;
+		int t = 0;
+		for(int i = s.length() - 1; i >= 0; i--){
+			num = num + (int)((s.charAt(i) - '0') * Math.pow(2, t));
+			t++;
+		}
+		return num;
+	}
+
+}
+
+class BinaryFormatException extends Exception{
+	public BinaryFormatException(){
+		super("Wrong Binary");
+	}
+}
