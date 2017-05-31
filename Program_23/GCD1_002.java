7af23dd476c354df86adfe04bf3ecbb9dcee429f
GX <luther0812@163.com>
2017-05-31 11:18:30 +0800

    Java Chapter 23

---
+++ Program_23/GCD1_002.java
@@ -0,0 +1,39 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Program_23;
+
+/**
+ * @author up
+ *
+ */
+import java.util.Scanner;
+
+public class GCD1_002 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter first integer: ");
+		int m = input.nextInt();
+		System.out.print("Enter second integer: ");
+		int n  = input.nextInt();
+		
+		System.out.println("The greatest common divisor for " + m + " and " + n + " is " + gcd(m, n));
+	}
+	
+	public static int gcd(int m, int n){
+		int gcd = 1;
+		if(m % n == 0){
+			return n;
+		}
+		
+		for(int k = n / 2; k >= 1; k--){
+			if(m % k == 0 && n % k == 0){
+				gcd = k;
+				break;
+			}
+		}
+		return gcd;
+	}
+
+}
---
+++ Program_23/PrimeNumbers_004.java
@@ -0,0 +1,47 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Program_23;
+
+/**
+ * @author up
+ *
+ */
+import java.util.Scanner;
+
+public class PrimeNumbers_004 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Find all prime numbers <= n, ebter n: ");
+		int n = input.nextInt();
+		final int NUMBER_PER_LINE = 10;
+		int count = 0;
+		int number = 2;
+		
+		System.out.println("The prime numbers are:");
+		
+		while(number <= n){
+			boolean isPrime = true;
+			
+			for(int divisor = 2; divisor <= (int)(Math.sqrt(number)); divisor++){
+				if(number % divisor == 0){
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if(isPrime){
+				count++;
+				if(count % NUMBER_PER_LINE == 0){
+					System.out.printf("%7d\n", number);
+				}else{
+					System.out.printf("%7d", number);
+				}
+			}
+			
+			number++;
+		}
+		System.out.println("\n" + count + " prime(s) less than n equal to " + n);
+	}
+
+}
