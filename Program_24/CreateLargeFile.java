5ec0d3f060b2331b3edb883e00acb313c42853e7
GX <luther0812@163.com>
2017-06-12 18:03:12 +0800

    Java Chapter 24

---
+++ Program_24/CreateLargeFile.java
@@ -0,0 +1,29 @@
+/**Language:Java Standard Edition
+ * Author:Luther
+ */
+package Program_24;
+
+/**
+ * @author up
+ *
+ */
+import java.io.*;
+
+public class CreateLargeFile {
+	public static void main(String[] args) throws Exception{
+		DataOutputStream output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("largedata.dat")));
+		for(int i = 0; i < 800004; i++){
+			output.writeInt((int)(Math.random() * 1000000));
+		}
+		output.close();
+		
+		DataInputStream input = new DataInputStream(new FileInputStream("largedata.dat"));
+		
+		for(int i = 0; i < 100; i++){
+			System.out.print(input.readInt() + " ");
+		}
+		
+		input.close();
+	}
+
+}
